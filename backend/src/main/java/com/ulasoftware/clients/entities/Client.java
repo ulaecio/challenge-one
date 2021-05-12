b43a5bb7fb3326fdd9cbc19a0668ed72853c3231
ulaecio <lenssonpol@hotmail.com>
2021-05-12 00:33:21 -0300

    Database seeding

--- backend/src/main/java/com/ulasoftware/clients/entities/Client.java
+++ backend/src/main/java/com/ulasoftware/clients/entities/Client.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="tb_product")
+@Table(name="tb_client")
 public class Client implements Serializable {
 	private static final long serialVersionUID = 1L;
 	

ddd61dd5326544178c81a9bc40e99880c8e50c41
ulaecio <lenssonpol@hotmail.com>
2021-05-25 22:17:26 -0300

    Update

--- backend/src/main/java/com/ulasoftware/clients/ClientApplication.java
+++ backend/src/main/java/com/ulasoftware/clients/ClientApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClientsApplication {
+public class ClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClientsApplication.class, args);
+		SpringApplication.run(ClientApplication.class, args);
 	}
 
 }
